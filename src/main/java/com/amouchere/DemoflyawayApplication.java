3c0ca828ea9dd75225ce9aa721aaa13dc4cd0199
MOUCHERE Antoine <antoine.mouchere@orange.com>
2017-01-17 15:52:34 +0100

    Give a try to flyway

---
+++ src/main/java/com/amouchere/DemoflyawayApplication.java
@@ -0,0 +1,12 @@
+package com.amouchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoflyawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoflyawayApplication.class, args);
+	}
+}
